4d0c69e635600a8693e3e21e668b4bf6c4237aaf
joe-tito <joetito1@gmail.com>
2025-01-16 23:37:10 -0500

    ok

--- output.tf
+++ output.tf
@@ -3,7 +3,7 @@ output "s3_url" {
   value       = "https://${aws_s3_bucket.this.id}.s3.${aws_s3_bucket.this.region}.amazonaws.com/index.html"
 }
 
-# output "ec2_public_dns" {
-#   description = "public dns for mongo ec2 instance"
-#   value       = aws_instance.mongo.public_dns
-# }
+output "ec2_public_dns" {
+  description = "public dns for mongo ec2 instance"
+  value       = aws_instance.mongo.public_dns
+}
