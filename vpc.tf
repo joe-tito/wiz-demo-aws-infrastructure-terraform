5776dbbe440ec9c0ac3a3d7bf011e052929c30e2
joe-tito <joetito1@gmail.com>
2025-01-17 12:00:57 -0500

    ok

--- vpc.tf
+++ vpc.tf
@@ -4,9 +4,9 @@ module "vpc" {
   name = "wiz-demo"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-east-1a", "eu-east-1b", "eu-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["es-east-1a", "es-east-1b", "es-east-1c"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
 
   #   enable_nat_gateway = true
   #   enable_vpn_gateway = true
