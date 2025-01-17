ee6b8956a75a64fcde4b8579c10e1848473edd42
joe-tito <joetito1@gmail.com>
2025-01-16 21:12:42 -0500

    ok

--- main.tf
+++ main.tf
@@ -17,6 +17,13 @@ resource "aws_s3_bucket" "this" {
   bucket = "wiz-demo-mongo-snapshots"
 }
 
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
@@ -26,12 +33,14 @@ resource "aws_s3_bucket_public_access_block" "this" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [
-    # aws_s3_bucket_ownership_controls.example,
-    aws_s3_bucket_public_access_block.this
-  ]
+resource "aws_s3_bucket_acl" "this" {
+
 
   bucket = aws_s3_bucket.this.id
   acl    = "public-read"
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.this,
+    aws_s3_bucket_public_access_block.this
+  ]
 }
