e0e096cda9b69f82b695a0291f2db606add09ad2
joe-tito <joetito1@gmail.com>
2025-01-16 21:08:16 -0500

    ok

--- main.tf
+++ main.tf
@@ -20,6 +20,18 @@ resource "aws_s3_bucket" "this" {
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls   = false
-  block_public_policy = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    # aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.this
+  ]
+
+  bucket = aws_s3_bucket.this.id
+  acl    = "public-read"
 }
