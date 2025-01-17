03985a50cbc763d9cf18f3657170207f60f8b939
joe-tito <joetito1@gmail.com>
2025-01-17 12:02:59 -0500

    ok

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "wiz-demo"
   cidr = "10.0.0.0/16"
 
-  azs             = ["es-east-1a", "es-east-1b", "es-east-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 
