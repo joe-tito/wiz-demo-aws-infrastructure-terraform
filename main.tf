ae6e630670cf1cdfc8c53e1de0c515bedc3e7b78
joe-tito <joetito1@gmail.com>
2025-01-16 21:21:24 -0500

    ok

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_s3_bucket" "this" {
 resource "aws_s3_bucket_ownership_controls" "this" {
   bucket = aws_s3_bucket.this.id
   rule {
-    object_ownership = "ObjectWriter"
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
@@ -35,7 +35,6 @@ resource "aws_s3_bucket_public_access_block" "this" {
 
 resource "aws_s3_bucket_acl" "this" {
 
-
   bucket = aws_s3_bucket.this.id
   acl    = "public-read"
 
@@ -43,4 +42,28 @@ resource "aws_s3_bucket_acl" "this" {
     aws_s3_bucket_ownership_controls.this,
     aws_s3_bucket_public_access_block.this
   ]
+}
+
+data "aws_iam_policy_document" "this" {
+  policy_id = "wiz-demo-public-read"
+
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject"
+    ]
+    effect = "Allow"
+    resources = [
+      "${aws_s3_bucket.this.arn}/*"
+    ]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.this.json
 }
