6b3be6481ff3818e22446f654e20d8686349e4cc
joe-tito <joetito1@gmail.com>
2025-01-16 23:13:21 -0500

    ok

--- ec2-mongo.tf
+++ ec2-mongo.tf
@@ -26,25 +26,25 @@ resource "aws_security_group" "this" {
   }
 }
 
-# resource "aws_instance" "mongo" {
+resource "aws_instance" "mongo" {
 
-#   ami                         = "ami-0e1bed4f06a3b463d" # Ubuntu 22.04 LTS
-#   instance_type               = "t2.micro"
-#   key_name                    = var.key_pair_name
-#   security_groups             = [aws_security_group.this.name]
-#   associate_public_ip_address = true
-#   user_data                   = <<-EOF
-#               #!/bin/bash
-#               apt-get update
-#               apt-get install gnupg curl
-#               curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
-#               gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-#               --dearmor
-#               echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-#               apt-get update
-#               apt-get install -y mongodb-org
-#               systemctl start mongod
-#               systemctl enable mongodb
-#               EOF
-#   depends_on                  = [aws_security_group.this]
-# }
+  ami                         = "ami-0e1bed4f06a3b463d" # Ubuntu 22.04 LTS
+  instance_type               = "t2.micro"
+  key_name                    = var.key_pair_name
+  security_groups             = [aws_security_group.this.name]
+  associate_public_ip_address = true
+  user_data                   = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install gnupg curl
+              curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
+              gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+              --dearmor
+              echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+              apt-get update
+              apt-get install -y mongodb-org
+              systemctl start mongod
+              systemctl enable mongodb
+              EOF
+  depends_on                  = [aws_security_group.this]
+}
--- output.tf
+++ output.tf
@@ -1,4 +1,9 @@
-output "url" {
+output "s3_url" {
   description = "url of the bucket"
   value       = "https://${aws_s3_bucket.this.id}.s3.${aws_s3_bucket.this.region}.amazonaws.com/index.html"
+}
+
+output "ec2_public_dns" {
+  description = "public dns for mongo ec2 instance"
+  value       = aws_instance.mongo.public_dns
 }
