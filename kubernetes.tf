98a90663c7638d5384cee6dec614496b43864a6c
joe-tito <joetito1@gmail.com>
2025-01-17 23:08:11 -0500

    ok

--- kubernetes.tf
+++ kubernetes.tf
@@ -8,17 +8,209 @@ resource "aws_ecr_repository" "this" {
   force_delete         = true
 }
 
-# module "ecs" {
-#     source = "terraform-aws-modules/ecs/aws"
-#     cluster_name = "wiz-demo-cluster"
-# }
-
-
-
-
 # # ########################
 # # # ECS cluster
 # # ########################
+
+module "ecs" {
+  source = "terraform-aws-modules/ecs/aws"
+
+  cluster_name = "wiz-demo-cluster"
+
+  cluster_configuration = {
+    execute_command_configuration = {
+      logging = "OVERRIDE"
+      log_configuration = {
+        cloud_watch_log_group_name = "/aws/ecs/aws-ec2"
+      }
+    }
+  }
+
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+    FARGATE_SPOT = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+  }
+
+  services = {
+
+    wiz-demo = {
+      cpu    = 1024
+      memory = 4096
+
+      # Container definition(s)
+      container_definitions = {
+
+        # fluent-bit = {
+        #   cpu       = 512
+        #   memory    = 1024
+        #   essential = true
+        #   image     = "906394416424.dkr.ecr.us-west-2.amazonaws.com/aws-for-fluent-bit:stable"
+        #   firelens_configuration = {
+        #     type = "fluentbit"
+        #   }
+        #   memory_reservation = 50
+        # }
+
+        web-app = {
+          cpu       = 512
+          memory    = 1024
+          essential = true
+          image     = aws_ecr_repository.this.repository_url
+          //image     = "public.ecr.aws/aws-containers/ecsdemo-frontend:776fd50"
+          port_mappings = [
+            {
+              containerPort = 3000
+              hostPort      = 3000
+            }
+          ]
+
+          #   # Example image used requires access to write to root filesystem
+          #   readonly_root_filesystem = false
+
+          #   dependencies = [{
+          #     containerName = "fluent-bit"
+          #     condition     = "START"
+          #   }]
+
+          #   enable_cloudwatch_logging = false
+          #   log_configuration = {
+          #     logDriver = "awsfirelens"
+          #     options = {
+          #       Name                    = "firehose"
+          #       region                  = "eu-west-1"
+          #       delivery_stream         = "my-stream"
+          #       log-driver-buffer-limit = "2097152"
+          #     }
+          #   }
+          memory_reservation = 100
+        }
+      }
+
+      service_connect_configuration = {
+        namespace = "example"
+        service = {
+          client_alias = {
+            port     = 80
+            dns_name = "ecs-sample"
+          }
+          port_name      = "ecs-sample"
+          discovery_name = "ecs-sample"
+        }
+      }
+
+      load_balancer = {
+        service = {
+          target_group_arn = module.alb.target_groups["ex_ecs"].arn
+          container_name   = "ecs-sample"
+          container_port   = 80
+        }
+      }
+
+      subnet_ids = module.vpc.private_subnets
+
+      security_group_rules = {
+        alb_ingress_3000 = {
+          type                     = "ingress"
+          from_port                = 80
+          to_port                  = 80
+          protocol                 = "tcp"
+          description              = "Service port"
+          source_security_group_id = "sg-12345678"
+        }
+        egress_all = {
+          type        = "egress"
+          from_port   = 0
+          to_port     = 0
+          protocol    = "-1"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      }
+    }
+  }
+}
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name = "wiz-demo-load-balancer"
+
+  load_balancer_type = "application"
+
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets
+
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = module.vpc.vpc_cidr_block
+    }
+  }
+
+  listeners = {
+    ex_http = {
+      port     = 80
+      protocol = "HTTP"
+
+      forward = {
+        target_group_key = "ex_ecs"
+      }
+    }
+  }
+
+  target_groups = {
+    ex_ecs = {
+      backend_protocol                  = "HTTP"
+      backend_port                      = 3000
+      target_type                       = "ip"
+      deregistration_delay              = 5
+      load_balancing_cross_zone_enabled = true
+
+      health_check = {
+        enabled             = true
+        healthy_threshold   = 5
+        interval            = 30
+        matcher             = "200"
+        path                = "/"
+        port                = "traffic-port"
+        protocol            = "HTTP"
+        timeout             = 5
+        unhealthy_threshold = 2
+      }
+
+      # Theres nothing to attach here in this definition. Instead,
+      # ECS will attach the IPs of the tasks to this target group
+      create_attachment = false
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
 
 # # resource "aws_ecs_cluster" "this" {
 # #   name = "wiz-demo-cluster"
