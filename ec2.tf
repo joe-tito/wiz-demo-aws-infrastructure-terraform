751e18553fbf9d87d6d99325505324c85f06c3e4
joe-tito <joetito1@gmail.com>
2025-01-18 14:14:03 -0500

    ok

--- ec2.tf
+++ ec2.tf
@@ -31,7 +31,7 @@ module "ec2_instance" {
         echo "use admin" >> /tmp/mongo-setup.js
         echo "db.createUser({ user: '${var.mongo_user}', pwd: '${var.mongo_password}', roles: ['root'] })" >> /tmp/mongo-setup.js
         echo "db.createCollection('reasons')" >> /tmp/mongo-setup.js
-        echo "db.reasons.insertMany([{reason: 'He built this super cool three-tiered web app'}, {reason: 'Everything is built as code with Terraform. How cool is that?'}, {reason: 'These reasons are stored & queried from Mongo}. {reason: 'He would add yet another, Joe, to the team!'}, {reason: 'He was obsessed with Wizards as a kid! Coincidence?'}])" >> /tmp/mongo-setup.js
+        echo "db.reasons.insertMany([{reason: 'He built this super cool three-tiered web app'}, {reason: 'Everything is built as code with Terraform. How cool is that?'}, {reason: 'These reasons are stored & queried from Mongo}, {reason: 'He would add yet another, Joe, to the team!'}, {reason: 'He was obsessed with Wizards as a kid! Coincidence?'}])" >> /tmp/mongo-setup.js
         mongosh < /tmp/mongo-setup.js
         rm /tmp/mongo-setup.js
 
