ba0656a3e21faa20a08236d9a70dc4a3dc548180
joe-tito <joetito1@gmail.com>
2025-01-19 13:39:56 -0500

    ok

--- scripts/backup_mongo.sh
+++ scripts/backup_mongo.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-BACKUP_FILE="./backup-$(date '+%Y-%m-%d-%H:%M:%S').gz"
-mongodump --uri="${LOCAL_MONGODB_URI}" --gzip --archive=${BACKUP_FILE}
-aws s3 cp ${BACKUP_FILE} s3://wiz-demo-mongo-snapshots
-rm ${BACKUP_FILE}
+# BACKUP_FILE="./backup-$(date '+%Y-%m-%d-%H:%M:%S').gz"
+mongodump --uri="${LOCAL_MONGODB_URI}" --gzip --archive="./backup.gz"
+aws s3 mv ./backup.gz s3://wiz-demo-mongo-snapshots/backup-$(date '+%Y-%m-%d-%H:%M:%S').gz
