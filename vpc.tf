5202fd6ffd8d4b7e45ec6bc004803a9a3e0e9896
joe-tito <joetito1@gmail.com>
2025-01-20 11:56:40 -0500

    ok

--- vpc.tf
+++ vpc.tf
@@ -13,12 +13,12 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   public_subnet_tags = {
-    kubernetes.io / role / elb                         = 1
+    "kubernetes.io/role/elb"                           = 1
     "kubernetes.io/cluster/${module.eks.cluster_name}" = 1
   }
 
   private_subnet_tags = {
-    kubernetes.io / role / internal-elb                = 1
+    "kubernetes.io/role/internal-elb"                  = 1
     "kubernetes.io/cluster/${module.eks.cluster_name}" = 1
   }
 
