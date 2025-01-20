51205fb8a66de1d1fd6b7efd9fb24ca4d9e3ae18
joe-tito <joetito1@gmail.com>
2025-01-19 20:35:59 -0500

    ok

--- ecs.tf
+++ ecs.tf
@@ -17,103 +17,103 @@ resource "aws_ecr_repository" "this" {
 # # ECS Cluster
 # ########################
 
-module "ecs" {
-  source = "terraform-aws-modules/ecs/aws"
+# module "ecs" {
+#   source = "terraform-aws-modules/ecs/aws"
 
-  cluster_name = "wiz-demo-cluster"
+#   cluster_name = "wiz-demo-cluster"
 
-  cluster_configuration = {
-    execute_command_configuration = {
-      logging = "OVERRIDE"
-      log_configuration = {
-        cloud_watch_log_group_name = "/aws/ecs/aws-ec2"
-      }
-    }
-  }
+#   cluster_configuration = {
+#     execute_command_configuration = {
+#       logging = "OVERRIDE"
+#       log_configuration = {
+#         cloud_watch_log_group_name = "/aws/ecs/aws-ec2"
+#       }
+#     }
+#   }
 
-  fargate_capacity_providers = {
-    FARGATE = {
-      default_capacity_provider_strategy = {
-        weight = 50
-      }
-    }
-    FARGATE_SPOT = {
-      default_capacity_provider_strategy = {
-        weight = 50
-      }
-    }
-  }
+#   fargate_capacity_providers = {
+#     FARGATE = {
+#       default_capacity_provider_strategy = {
+#         weight = 50
+#       }
+#     }
+#     FARGATE_SPOT = {
+#       default_capacity_provider_strategy = {
+#         weight = 50
+#       }
+#     }
+#   }
 
-  services = {
+#   services = {
 
-    wiz-demo-web-app = {
-      cpu    = 1024
-      memory = 4096
+#     wiz-demo-web-app = {
+#       cpu    = 1024
+#       memory = 4096
 
-      container_definitions = {
+#       container_definitions = {
 
-        (local.container_name) = {
-          cpu       = 512
-          memory    = 1024
-          essential = true
-          image     = aws_ecr_repository.this.repository_url
-          port_mappings = [
-            {
-              name          = local.container_name
-              containerPort = local.container_port
-              hostPort      = local.container_port
-              protocol      = "tcp"
-            }
-          ]
+#         (local.container_name) = {
+#           cpu       = 512
+#           memory    = 1024
+#           essential = true
+#           image     = aws_ecr_repository.this.repository_url
+#           port_mappings = [
+#             {
+#               name          = local.container_name
+#               containerPort = local.container_port
+#               hostPort      = local.container_port
+#               protocol      = "tcp"
+#             }
+#           ]
 
-          readonly_root_filesystem = false
+#           readonly_root_filesystem = false
 
-          memory_reservation = 100
-        }
-      }
+#           memory_reservation = 100
+#         }
+#       }
 
-      service_connect_configuration = {
-        namespace = aws_service_discovery_http_namespace.this.arn
-        service = {
-          client_alias = {
-            port     = local.container_port
-            dns_name = local.container_name
-          }
-          port_name      = local.container_name
-          discovery_name = local.container_name
-        }
-      }
+#       service_connect_configuration = {
+#         namespace = aws_service_discovery_http_namespace.this.arn
+#         service = {
+#           client_alias = {
+#             port     = local.container_port
+#             dns_name = local.container_name
+#           }
+#           port_name      = local.container_name
+#           discovery_name = local.container_name
+#         }
+#       }
 
-      load_balancer = {
-        service = {
-          target_group_arn = module.alb.target_groups["ex_ecs"].arn
-          container_name   = local.container_name
-          container_port   = local.container_port
-        }
-      }
+#       load_balancer = {
+#         service = {
+#           target_group_arn = module.alb.target_groups["ex_ecs"].arn
+#           container_name   = local.container_name
+#           container_port   = local.container_port
+#         }
+#       }
 
-      subnet_ids = module.vpc.private_subnets
+#       subnet_ids = module.vpc.private_subnets
 
-      security_group_rules = {
-        alb_ingress_3000 = {
-          type                     = "ingress"
-          from_port                = local.container_port
-          to_port                  = local.container_port
-          protocol                 = "tcp"
-          description              = "Service port"
-          source_security_group_id = module.alb.security_group_id
-        }
-        egress_all = {
-          type        = "egress"
-          from_port   = 0
-          to_port     = 0
-          protocol    = "-1"
-          cidr_blocks = ["0.0.0.0/0"]
-        }
-      }
-    }
-  }
-}
+#       security_group_rules = {
+#         alb_ingress_3000 = {
+#           type                     = "ingress"
+#           from_port                = local.container_port
+#           to_port                  = local.container_port
+#           protocol                 = "tcp"
+#           description              = "Service port"
+#           source_security_group_id = module.alb.security_group_id
+#         }
+#         egress_all = {
+#           type        = "egress"
+#           from_port   = 0
+#           to_port     = 0
+#           protocol    = "-1"
+#           cidr_blocks = ["0.0.0.0/0"]
+#         }
+#       }
+#     }
+#   }
+# }
 
 ########################
 # Load Balancer
