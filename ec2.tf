4a573c594884a7fb22ca51911914a25c56efa2ca
joe-tito <joetito1@gmail.com>
2025-01-19 19:52:12 -0500

    ok

--- ec2.tf
+++ ec2.tf
@@ -61,66 +61,6 @@ module "ec2_instance" {
         EOF
 }
 
-# module "ec2_instance_public" {
-#   source = "terraform-aws-modules/ec2-instance/aws"
-
-#   name                        = "ec2-mongo-public"
-#   instance_type               = "t2.micro"
-#   ami                         = "ami-0e1bed4f06a3b463d" # Ubuntu 22.04 LTS
-#   key_name                    = var.key_pair_name
-#   subnet_id                   = module.vpc.public_subnets[0]
-#   associate_public_ip_address = true
-#   iam_instance_profile        = aws_iam_instance_profile.admin_profile.name
-#   vpc_security_group_ids = [
-#     aws_security_group.ingress_mongo_all.id,
-#     aws_security_group.ingress_ssh_all.id,
-#     aws_security_group.egress_internet.id
-#   ]
-
-#   user_data = <<-EOF
-#         #!/bin/bash
-#         apt-get update
-#         apt-get install gnupg curl
-#         curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
-#         gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-#         --dearmor
-#         echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-#         apt-get update
-#         apt-get install -y mongodb-org unzip
-
-#         systemctl start mongod
-#         systemctl unmask mongodb
-#         systemctl enable mongodb
-#         sleep 30
-
-#         echo "use admin" >> /tmp/mongo-setup.js
-#         echo "db.createUser({ user: '${var.mongo_user}', pwd: '${var.mongo_password}', roles: ['root'] })" >> /tmp/mongo-setup.js
-#         echo "db.createCollection('reasons')" >> /tmp/mongo-setup.js
-#         echo "db.reasons.insertMany([{reason: 'He built this super cool three-tiered web app'}, {reason: 'Everything is built as code with Terraform. How cool is that?'}, {reason: 'These reasons are stored in, and queried from, MongoDB'}, {reason: 'He would add yet another, Joe, to the team!'}, {reason: 'He was obsessed with Wizards as a kid! Coincidence?'}])" >> /tmp/mongo-setup.js
-#         mongosh < /tmp/mongo-setup.js
-#         rm /tmp/mongo-setup.js
-
-#         echo "security:" >> /etc/mongod.conf
-#         echo "    authorization: enabled" >> /etc/mongod.conf
-
-#         sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-
-#         systemctl restart mongod
-
-#         curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-#         unzip awscliv2.zip
-#         sudo ./aws/install
-
-#         echo "#!/bin/bash" >> /home/ubuntu/backup_mongo.sh
-#         echo "mongodump --uri=\"mongodb://${var.mongo_user}:${var.mongo_password}@localhost:27017\" --gzip --archive=backup.gz" >> /home/ubuntu/backup_mongo.sh
-#         echo "aws s3 mv ./backup.gz s3://wiz-demo-mongo-snapshots/backup-\$(date '+%Y-%m-%d-%H:%M:%S').gz" >> /home/ubuntu/backup_mongo.sh
-#         chmod u+x /home/ubuntu/backup_mongo.sh
-
-#         echo "*/5 * * * * root /home/ubuntu/backup_mongo.sh" >> /etc/crontab
-
-#         EOF
-# }
-
 resource "aws_iam_instance_profile" "admin_profile" {
   name = "admin_profile"
   role = aws_iam_role.admin_role.name
