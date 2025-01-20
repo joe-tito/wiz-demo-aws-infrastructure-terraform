47cbdf831d1d0f7d96bd1838ae226c62542af8c1
joe-tito <joetito1@gmail.com>
2025-01-20 12:16:23 -0500

    ok

--- eks.tf
+++ eks.tf
@@ -10,8 +10,8 @@ module "eks" {
   cluster_version = "1.31"
 
   vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.public_subnets
-  control_plane_subnet_ids = module.vpc.public_subnets
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
 
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
