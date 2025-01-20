3a2a8b9721a7d607653e5b112ca89f4282f0ed6b
joe-tito <joetito1@gmail.com>
2025-01-19 21:54:18 -0500

    ok

--- eks.tf
+++ eks.tf
@@ -9,26 +9,4 @@ module "eks" {
 
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
-
-  cluster_compute_config = {
-    enabled    = true
-    node_pools = ["general-purpose"]
-  }
-
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-  }
-
-  eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-    }
-  }
 }
