664042764a99637b15424db5a393823e384e3464
joe-tito <joetito1@gmail.com>
2025-01-21 18:33:01 -0500

    ok

--- s3.tf
+++ s3.tf
@@ -7,52 +7,52 @@ resource "aws_s3_bucket" "this" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_ownership_controls" "this" {
-  bucket = aws_s3_bucket.this.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
+# resource "aws_s3_bucket_ownership_controls" "this" {
+#   bucket = aws_s3_bucket.this.id
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
 
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls = false
-  #block_public_policy = false
+  #block_public_acls = false
+  block_public_policy = false
   #ignore_public_acls      = false
   #restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "this" {
+# resource "aws_s3_bucket_acl" "this" {
 
-  bucket = aws_s3_bucket.this.id
-  acl    = "public-read"
+#   bucket = aws_s3_bucket.this.id
+#   acl    = "public-read"
 
-  depends_on = [
-    aws_s3_bucket_ownership_controls.this,
-    aws_s3_bucket_public_access_block.this
-  ]
+#   depends_on = [
+#     aws_s3_bucket_ownership_controls.this,
+#     aws_s3_bucket_public_access_block.this
+#   ]
+# }
+
+data "aws_iam_policy_document" "this" {
+  policy_id = "wiz-demo-public-read"
+
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+    effect = "Allow"
+    resources = [
+      "${aws_s3_bucket.this.arn}/*"
+    ]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
 }
 
-# data "aws_iam_policy_document" "this" {
-#   policy_id = "wiz-demo-public-read"
-
-#   statement {
-#     actions = [
-#       "s3:GetObject"
-#     ]
-#     effect = "Allow"
-#     resources = [
-#       "${aws_s3_bucket.this.arn}/*"
-#     ]
-#     principals {
-#       type        = "*"
-#       identifiers = ["*"]
-#     }
-#   }
-# }
-
-# resource "aws_s3_bucket_policy" "this" {
-#   bucket = aws_s3_bucket.this.id
-#   policy = data.aws_iam_policy_document.this.json
-# }
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.this.json
+}
