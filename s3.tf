92569ba60f6966a25acef6c143103ef9ddffde58
joe-tito <joetito1@gmail.com>
2025-01-21 18:28:08 -0500

    ok

--- s3.tf
+++ s3.tf
@@ -17,10 +17,10 @@ resource "aws_s3_bucket" "this" {
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls   = false
-  block_public_policy = false
+  block_public_acls = false
+  #block_public_policy = false
   #ignore_public_acls      = false
-  restrict_public_buckets = false
+  #restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_acl" "this" {
@@ -34,25 +34,25 @@ resource "aws_s3_bucket_acl" "this" {
   ]
 }
 
-data "aws_iam_policy_document" "this" {
-  policy_id = "wiz-demo-public-read"
+# data "aws_iam_policy_document" "this" {
+#   policy_id = "wiz-demo-public-read"
 
-  statement {
-    actions = [
-      "s3:GetObject"
-    ]
-    effect = "Allow"
-    resources = [
-      "${aws_s3_bucket.this.arn}/*"
-    ]
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-  }
-}
+#   statement {
+#     actions = [
+#       "s3:GetObject"
+#     ]
+#     effect = "Allow"
+#     resources = [
+#       "${aws_s3_bucket.this.arn}/*"
+#     ]
+#     principals {
+#       type        = "*"
+#       identifiers = ["*"]
+#     }
+#   }
+# }
 
-resource "aws_s3_bucket_policy" "this" {
-  bucket = aws_s3_bucket.this.id
-  policy = data.aws_iam_policy_document.this.json
-}
+# resource "aws_s3_bucket_policy" "this" {
+#   bucket = aws_s3_bucket.this.id
+#   policy = data.aws_iam_policy_document.this.json
+# }
