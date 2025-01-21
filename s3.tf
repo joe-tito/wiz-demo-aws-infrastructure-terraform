98d46c0f44d3d2222e9be42afafaf33e8ccad90b
joe-tito <joetito1@gmail.com>
2025-01-21 18:16:58 -0500

    ok

--- s3.tf
+++ s3.tf
@@ -7,6 +7,33 @@ resource "aws_s3_bucket" "this" {
   force_destroy = true
 }
 
+# resource "aws_s3_bucket_ownership_controls" "this" {
+#   bucket = aws_s3_bucket.this.id
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  #block_public_acls = false
+  block_public_policy = false
+  #ignore_public_acls      = false
+  #restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "this" {
+
+  bucket = aws_s3_bucket.this.id
+  acl    = "public-read"
+
+  depends_on = [
+    # aws_s3_bucket_ownership_controls.this,
+    aws_s3_bucket_public_access_block.this
+  ]
+}
+
 data "aws_iam_policy_document" "this" {
   policy_id = "wiz-demo-public-read"
 
